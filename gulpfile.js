88110830cc7ae84a9951e18cef11b06a3bc853cd
marcs <m@marcsist.com>
2021-01-06 23:23:35 +0100

    added gulp-sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const GulpClient = require("gulp");
 const { watch, series, src, dest } = require("gulp");
 const sass = require("gulp-sass");
 const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
 
 sass.complier = require(`node-sass`);
 
@@ -9,8 +10,14 @@ function runSass() {
   // place code for your default task here
   // we want to run "css css/app.scss app.css --watch"
   return src("css/app.scss")
+  .pipe(sourcemaps.init())
   .pipe(sass())
-  .pipe(cleanCSS())
+  .pipe(
+    cleanCSS({
+      compatibility: 'ie8'
+    })
+  )
+  .pipe(sourcemaps.write())
   .pipe(dest("."));
 }
 

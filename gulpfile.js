100136cc3b80613dd71973139f726ba98dc83c5d
marcs <m@marcsist.com>
2021-01-06 14:12:29 +0100

    seperated out sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+
+gulp.task("sass", function() {
+      // we want to run "sass sass css/app.scss app.css --watch"
+      return gulp.src('css/app.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('.'))
+})
+
+
 function defaultTask(cb) {
-    // place code for your default task here
+
+
     cb();
   }
   
